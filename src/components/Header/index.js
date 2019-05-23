6d591c2cbb402ab32910d87d390685c5f37f63c8
SathishSaminathan <Sathishcse1975@gmail.com>
2019-05-23 12:38:40 +0530

    initial_commit

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,25 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+
+import "./Header.css";
+import { Button } from "semantic-ui-react";
 
 class Header extends Component {
-    render() {
-        return (
-            <div>
-                Header
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="header_container">
+        <ul>
+          {/* <li>
+            <a>
+              <img className="profile_pic" src="https://randomuser.me/api/portraits/men/35.jpg" />
+            </a>
+          </li> */}
+          <li>
+            <Button primary>Login</Button>
+          </li>
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default Header;
