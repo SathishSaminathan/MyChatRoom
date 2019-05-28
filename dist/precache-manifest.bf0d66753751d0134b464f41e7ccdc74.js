ad273b0d270c12366c345bf2b257a111860d18af
SathishSaminathan <Sathishcse1975@gmail.com>
2019-05-28 16:43:53 +0530

    commit_for_first_release

---
+++ dist/precache-manifest.bf0d66753751d0134b464f41e7ccdc74.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b9c52e5ffaeedfe6a0c1dcc520f6570",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7cf95b3fa2567240c60",
+    "url": "/static/css/2.39017165.chunk.css"
+  },
+  {
+    "revision": "66637468b0538f97f9f8",
+    "url": "/static/css/main.f1023ca2.chunk.css"
+  },
+  {
+    "revision": "b7cf95b3fa2567240c60",
+    "url": "/static/js/2.17c65964.chunk.js"
+  },
+  {
+    "revision": "66637468b0538f97f9f8",
+    "url": "/static/js/main.b2f332de.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "451e317dd6253a9fb93e5e96559d0107",
+    "url": "/static/media/logo.451e317d.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
